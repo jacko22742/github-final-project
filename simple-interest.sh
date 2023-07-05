cf3a5992078a27d32d0aebb422cdc1591719dd18
jacko22742 <107209078+jacko22742@users.noreply.github.com>
2023-07-05 18:05:46 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # jacko22742
    # Input:
    # p, principal amount
    # t, time period in years
